a249f51bf7c63f140c271989e8c5815a4b0040d0
Lucas Paiva <56031422+lkpaiva@users.noreply.github.com>
2023-10-16 16:20:58 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
-
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   String _infoText = "Informe seus dados";
@@ -35,15 +34,19 @@ class _HomeState extends State<Home> {
 
       double imc = weight / (height * height);
       print(imc);
-      if (imc < 18.6) {
-        _infoText = "O Abaixo do Peso (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 29.9 && imc < 34.9) {
+      if (imc < 16.0) {
+        _infoText = "Magreza grave (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 16.0 && imc < 17.0) {
+        _infoText = "Magreza moderada (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 17.0 && imc < 18.5) {
+        _infoText = "Magreza leve (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.5 && imc < 25.0) {
+        _infoText = "Saudável (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 25.0 && imc < 30.0) {
+        _infoText = "Sobrepeso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 30 && imc < 35.0) {
         _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 34.9 && imc < 39.9) {
+      } else if (imc >= 35.0 && imc < 40.0) {
         _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
       } else if (imc >= 40.0) {
         _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
@@ -79,8 +82,7 @@ class _HomeState extends State<Home> {
                 keyboardType: TextInputType.text,
                 decoration: const InputDecoration(
                     labelText: "Nome completo",
-                    labelStyle: TextStyle(color: Colors.black87)
-                ),
+                    labelStyle: TextStyle(color: Colors.black87)),
                 textAlign: TextAlign.center,
                 style: const TextStyle(color: Colors.black, fontSize: 25.0),
               ),
